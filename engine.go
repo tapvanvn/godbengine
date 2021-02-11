0d7bb77f6d36f44861a0d0866a2ecf4fa68b5144
tapvanvn <tapvanvn@gmail.com>
2021-02-11 11:09:00 +0700

    fixbug

--- engine.go
+++ engine.go
@@ -13,49 +13,14 @@ func GetEngine() *engine.Engine {
 
 	if uniqueEngine == nil {
 
-		uniqueEngine = &engine.Engine{}
+		testEngine := &engine.Engine{}
 
 		if InitEngineFunc != nil {
 
 			InitEngineFunc(uniqueEngine)
 		}
+
+		uniqueEngine = testEngine
 	}
 	return uniqueEngine
-}
-
-//Start start engine
-func Start(engine *engine.Engine) {
-	/*
-		//read redis define from env
-		redisConnectString := utility.GetEnv("CONNECT_STRING_REDIS")
-		fmt.Println("redis:", redisConnectString)
-		redisPool := adapter.RedisPool{}
-
-		err := redisPool.Init(redisConnectString)
-
-		if err != nil {
-
-			panic("cannot init redis")
-		}
-
-		//read mongodb define
-		mongoConnectString := utility.GetEnv("CONNECT_STRING_DOCUMENTDB")
-		databaseName := utility.GetEnv("DOCUMENTDB_DATABASE")
-		fmt.Println("mongo:", mongoConnectString)
-		mongoPool := adapter.MongoPool{}
-
-		err = mongoPool.InitWithDatabase(mongoConnectString, databaseName)
-
-		if err != nil {
-
-			panic("cannot init mongo")
-		}
-
-		//mongo file pool
-		fileMongoPool := adapter.MongoFilePool{}
-
-		fileMongoPool.Init("file", &mongoPool)
-
-		engine.Init(&redisPool, &mongoPool, &fileMongoPool)
-	*/
 }
